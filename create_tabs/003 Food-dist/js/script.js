9dce7443e4cfc2cae24022cbfac666558b55167e
Roman <rombom24@gmail.com>
2022-09-05 12:23:04 +0300

    add eventList click

--- create_tabs/003 Food-dist/js/script.js
+++ create_tabs/003 Food-dist/js/script.js
@@ -1,1 +1,38 @@
-alert(3)
+window.addEventListener('DOMContentLoaded', () => {
+
+const tabs = document.querySelectorAll('.tabheader__item'),
+      tabsContent = document.querySelectorAll('.tabcontent'),
+      tabsParent = document.querySelector('.tabheader__items');
+
+function hideTabComtent() {
+    tabsContent.forEach(el => el.style.display = 'none');
+    tabs.forEach(el => el.classList.remove('tabheader__item_active'));
+}
+
+console.dir(tabsParent);
+
+function showTabContent(i=0) {
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+}
+
+hideTabComtent();
+showTabContent();
+
+tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, index) => { 
+        if (target === item) {
+            hideTabComtent();
+            showTabContent(index);
+        }    
+    });
+    }
+});
+console.log(tabs);
+
+
+
+});
