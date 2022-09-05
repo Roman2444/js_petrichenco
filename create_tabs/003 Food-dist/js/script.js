d11bbc2fc43bb9806e882844a988462a399807fb
Roman <rombom24@gmail.com>
2022-09-05 13:04:27 +0300

    add css class show-hide and duration

--- create_tabs/003 Food-dist/js/script.js
+++ create_tabs/003 Food-dist/js/script.js
@@ -1,37 +1,45 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-const tabs = document.querySelectorAll('.tabheader__item'),
-      tabsContent = document.querySelectorAll('.tabcontent'),
-      tabsParent = document.querySelector('.tabheader__items');
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-function hideTabComtent() {
-    tabsContent.forEach(el => el.style.display = 'none');
-    tabs.forEach(el => el.classList.remove('tabheader__item_active'));
-}
+    function hideTabComtent() {
+        tabsContent.forEach(el => {
+            el.classList.add('hide');
+            el.classList.remove('show', 'fade');
+        });
 
-console.dir(tabsParent);
-
-function showTabContent(i=0) {
-    tabsContent[i].style.display = 'block';
-    tabs[i].classList.add('tabheader__item_active');
-}
-
-hideTabComtent();
-showTabContent();
-
-tabsParent.addEventListener('click', (event) => {
-    const target = event.target;
-
-    if (target.classList.contains('tabheader__item')) {
-        tabs.forEach((item, index) => { 
-        if (target === item) {
-            hideTabComtent();
-            showTabContent(index);
-        }    
-    });
+        tabs.forEach(el => {
+            el.classList.remove('tabheader__item_active');
+        });
+        
     }
-});
-console.log(tabs);
+
+    console.dir(tabsParent);
+
+    function showTabContent(i=0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabComtent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, index) => { 
+            if (target === item) {
+                hideTabComtent();
+                showTabContent(index);
+                }    
+            });
+        }
+    });
+    console.log(tabs);
 
 
 
