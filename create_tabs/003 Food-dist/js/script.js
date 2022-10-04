2044a9bd9d5e7617734031de4b0bed377c4c33ac
Roman <rombom24@gmail.com>
2022-10-04 21:49:47 +0300

    add classes for menu

--- create_tabs/003 Food-dist/js/script.js
+++ create_tabs/003 Food-dist/js/script.js
@@ -163,14 +163,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', showModalByScroll);
 
+    //  созданы классы для меню
+    // =========================
+
     class MenuCard {
-        constructor(title, src, alt, descr, price, parentSelector) {
+        constructor(title, src, alt, descr, price, parentSelector, ...classes) {
             this.title = title;
             this.src = src;
             this.alt = alt;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
             this.transfer = 27; 
             this.changeToUAH();
         }
@@ -179,8 +183,14 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         render() {
             const element = document.createElement('div');
+            
+            if (this.classes.length == '0') {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+            this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML = `
-                <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -189,10 +199,8 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
             `;
             this.parent.append(element);
-
         }
     }
 
@@ -202,7 +210,9 @@ window.addEventListener('DOMContentLoaded', () => {
         "vegy",
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu__field .container'
+        '.menu__field .container',
+        'menu__item'
+        
     ).render();
 
     new MenuCard(
@@ -211,7 +221,8 @@ window.addEventListener('DOMContentLoaded', () => {
         "post",
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         10,
-        '.menu__field .container'
+        '.menu__field .container',
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -220,7 +231,72 @@ window.addEventListener('DOMContentLoaded', () => {
         "elite",
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         12,
-        '.menu__field .container'
+        '.menu__field .container',
+        'menu__item'
     ).render();
+
+
+    // Forms
+    //==========
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            // для данных FormData заголовок не нужен
+            // request.setRequestHeader('Content-type', 'application/json');
+
+
+            const formData = new FormData(form);
+
+            // если передаем данные в формате JSON
+            //======================================
+            // const obj = {};
+            // formData.forEach(function(value, key) {
+            //     obj[key] = value;
+            // });
+
+            // const json = JSON.stringify(obj);
+            // request.send(json);
+
+
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response)
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+
+        });
+    }
+
 
 });
