f64bf40a4c800ff091e2fbcc943b457a107b6e86
Roman <rombom24@gmail.com>
2022-10-13 19:20:37 +0300

    add calc

--- create_tabs/Food-dist/js/script.js
+++ create_tabs/Food-dist/js/script.js
@@ -449,5 +449,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
+    // Calc
+    // ========
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, ratio, age;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !ratio || !age) {
+            result.textContent = '____';
+            return;
+        }
+        if (sex === 'female') {
+            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+        } else {
+            result.textContent =  (88.36 + (13.4 * weight) + (4.8 *height) - (5.7 * age)) * ratio;
+        }
+
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        document.querySelector(parentSelector).addEventListener('click', (e)=> {
+            if (e.target.getAttribute('data-ratio')) {
+                ratio = +e.target.getAttribute('data-ratio');
+            } else {
+                sex = e.target.getAttribute('id');
+            }
+
+
+            elements.forEach(el => el.classList.remove(activeClass));
+
+            
+            e.target.classList.add(activeClass);
+            console.log(e.target);
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
 
 });
