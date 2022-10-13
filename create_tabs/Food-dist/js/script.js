a451284df110ec433caa613b6e4945bf594645f7
Roman <rombom24@gmail.com>
2022-10-13 16:06:43 +0300

    added reg expr - func onlyNumbers

--- create_tabs/Food-dist/js/script.js
+++ create_tabs/Food-dist/js/script.js
@@ -339,6 +339,10 @@ window.addEventListener('DOMContentLoaded', () => {
         dots[slideIndex - 1].style.opacity = 1;
     }
 
+    function onlyNumbers(str) {
+       return +str.replace(/\D/g, '');
+    }
+
     slidesField.style.width = 100 * slides.length + '%'; 
     slidesField.style.display = 'flex';
     slidesField.style.transition = '0.5s all';
@@ -395,12 +399,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     nextSlid.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == onlyNumbers(width) * (slides.length - 1)) {
             offset = 0;
             slideIndex = 1;
             
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += onlyNumbers(width);
             slideIndex++;
         }
 
@@ -413,11 +417,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prevSlid.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = onlyNumbers(width) * (slides.length - 1);
             slideIndex = slides.length ;
             
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= onlyNumbers(width);
             slideIndex--;
         }
 
@@ -433,7 +437,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
     
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = onlyNumbers(width) * (slideTo - 1);
 
             sliderTransform();
 
@@ -445,5 +449,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
-    
+
 });
